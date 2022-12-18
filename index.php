48ac5c190ee50d978a610884ba73d90fe45adf72
Emmedson <livinusdogz@gmail.com>
2022-12-18 17:10:51 +0100

    test task

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
     <style>
         h1{
             font-size: 24px;
@@ -18,6 +19,20 @@
 <h2>Hello world</h2>
     
 <?php
+
+for ($i=1; $i < 5; $i++)
+{
+        echo "$i <br>";
+}
+
+$products = ['Car', 'Bags', 'sheos'];
+
+foreach($products as $product){
+    echo " The product is $product ";
+}
+
+
+
 echo "This is a php line";
 ?>
 
